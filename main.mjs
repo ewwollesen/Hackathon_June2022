9c73e251c93380fb9711fa065ee273b7214ec7e9
Richard Braley <zippyllama@gmail.com>
2022-06-23 17:51:14 -0500

    esm-adjustment-node-fetch-add

---
+++ main.mjs
@@ -0,0 +1,42 @@
+//init
+// moved to ESM const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+//express init
+// moved to ESM const express = require('express');
+import express from 'express';
+const app = express();
+const port = process.env.port | 8080;
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+app.post('/', (req, res) => {
+    var body = req.body;
+    console.log('Received post', body);
+    fetchBoards();
+    res.sendStatus(200);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
+
+
+//TODO : Class/Mod later
+import fetch from 'node-fetch';
+function fetchBoards() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + process.env.personalAccessToken,
+            'X-Requested-With':'XMLHttpRequest'
+        }
+    }
+    fetch(process.env.MMURL + 'plugins/focalboard/api/v1/workspaces')
+        .then(res => res.text())
+        .then(resText => console.log(resText));
+}
