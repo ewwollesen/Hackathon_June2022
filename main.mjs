9b5684ad979f9ecab06fc1ce49b107391b21cd2c
Richard Braley <zippyllama@gmail.com>
2022-06-24 16:18:21 -0500

    post-boards-fetch-playbook-runs

--- main.mjs
+++ main.mjs
@@ -7,6 +7,133 @@ import { eachDayOfInterval } from 'date-fns';
 // const testuid = uuidv4().replaceAll('-', '').substring(0, 26);
 // console.log(testuid);
 
+//*****************************************************************************************************/
+//TODO : Classes file
+class Board {
+  constructor(boardTitle) {
+    this.ids = {
+      "boardId" : returnNewUUID(),
+      "viewBlockId" : returnNewUUID(),
+      "viewCardLT7DaysId" : returnNewUUID(),
+      "viewCardLT14DaysId" : returnNewUUID(),
+      "viewCardGT14DaysId" : returnNewUUID(),
+      "selectCardPropertyId" : returnNewUUID(),
+      "selectCardOverdueId" : returnNewUUID(),
+      "selectCardCurrentId" : returnNewUUID(),
+      "channelURLId" : returnNewUUID(),
+      "ownerPlaybookId" : returnNewUUID()
+    }
+
+    this.id = this.ids.boardId;
+    this.schema = 1;
+    this.workspaceId="";
+    this.parentId="";
+    this.rootId = this.ids.boardId; 
+    this.createdBy="";
+    this.modifiedBy="";
+    this.type="board";
+    this.fields = {
+      "showDescription":false,
+        "description":"",
+        "icon":"",
+        "isTemplate":false,
+        "templateVer":0,
+        "columnCalculations":[
+           
+        ],
+        "cardProperties":[
+           {
+              "id" : this.ids.viewBlockId, 
+              "name":"Status",
+              "type":"select",
+              "options":[
+                {
+                    "id" : this.ids.viewCardLT7DaysId, 
+                    "value":"> week",
+                    "color":"propColorGreen"
+                },
+                {
+                    "id" : this.ids.viewCardLT14DaysId, //card option id 2
+                    "value":"1-2 weeks",
+                    "color":"propColorYellow"
+                },
+                {
+                    "id" : this.ids.viewCardGT14DaysId, // card option id 3
+                    "value":"> 2 weeks",
+                    "color":"propColorRed"
+                }
+              ]
+           },
+           {
+                "id" : this.ids.selectCardPropertyId, //select card property ID
+                "name": "Update Status",
+                "type": "select",
+                "options": [
+                    {
+                        "color": "propColorRed",
+                        "id" : this.ids.selectCardOverdueId, //overdue select property ID
+                        "value": "Overdue"
+                    },
+                    {
+                        "color": "propColorGreen",
+                        "id" : this.ids.selectCardCurrentId, //current select property ID
+                        "value": "Current"
+                    }
+                ]               
+            },
+            {
+                "id" : this.ids.channelURLId, //URL property ID
+                "name": "Channel URL",
+                "options": [],
+                "type": "url"
+            },
+            {
+                "id" : this.ids.ownerPlaybookId, //owner property ID
+                "name": "Person",
+                "options": [],
+                "type": "person"
+           }
+        ]
+    };
+    this.title = boardTitle;
+    this.createAt = new Date();
+    this.updateAt = new Date();
+    this.deleteAt = 0;
+    this.limited = false
+  }
+}
+
+class Card {
+  constructor(boardId, viewBlockId, viewCardId, title) {
+    this.id = returnNewUUID();
+    this.schema = 1;
+    this.workspaceId="";
+    this.parentId=boardId;
+    this.rootId=boardId; 
+    this.createdBy="";
+    this.modifiedBy="";
+    this.type="card";
+    this.fields = {
+      "icon":"",
+      "properties":{
+        groupCardId:viewCardId
+      },
+      "contentOrder":[],
+      "isTemplate":false
+    };
+    this.title = title;
+    this.createAt = new Date();
+    this.updateAt = new Date();
+    this.deleteAt = 0;
+    this.limited = false
+  }
+}
+
+//*****************************************************************************************************/
+
+//*****************************************************************************************************/
+//*** INIT                                                                                           
+//*****************************************************************************************************/
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
@@ -19,12 +146,17 @@ const port = process.env.port | 8080;
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
+//*****************************************************************************************************/
+//*** Routes                                                                                           
+//*****************************************************************************************************/
+
 app.post('/', (req, res) => {
   var body = req.body;
   console.log('Received post', body);
   fetchWorkspace()
-    .then((res) => fetchPlaybookRuns(res))
-    .then((res) => createBoard(res));
+  .then((res) => createBoard(res))  
+  .then((res) => fetchPlaybookRuns(res))
+  //.then((res) => createCards(res))
   res.sendStatus(200);
 });
 
@@ -32,7 +164,12 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
+
 //TODO : Class/Mod later
+//*****************************************************************************************************/
+//*** Application                                                                                           
+//*****************************************************************************************************/
+
 import fetch from 'node-fetch';
 function fetchWorkspace() {
   const options = {
@@ -63,71 +200,7 @@ function fetchWorkspace() {
 }
 
 function createBoard(workspace) {
-  const board = [
-    {
-      id: '7zs1f33uoych1c6ucd5t77q4f3x',
-      schema: 1,
-      workspaceId: '',
-      parentId: '',
-      rootId: '7zs1f33uoych1c6ucd5t77q4f3x',
-      createdBy: '',
-      modifiedBy: '',
-      type: 'board',
-      fields: {
-        showDescription: false,
-        description: '',
-        icon: '',
-        isTemplate: false,
-        templateVer: 0,
-        columnCalculations: [],
-        cardProperties: [
-          {
-            id: 'azr3oh9ko8hc5yfsmapchkpmp4x',
-            name: 'Status',
-            type: 'select',
-            options: []
-          }
-        ]
-      },
-      title: 'Open Playbooks Dashboard',
-      createAt: 1655409199321,
-      updateAt: 1655409199321,
-      deleteAt: 0,
-      limited: false
-    },
-    {
-      id: '735bmk4s7sy31tieei6pgzuxw8x',
-      schema: 1,
-      workspaceId: '',
-      parentId: '7zs1f33uoych1c6ucd5t77q4f3x',
-      rootId: '7zs1f33uoych1c6ucd5t77q4f3x',
-      createdBy: '',
-      modifiedBy: '',
-      type: 'view',
-      fields: {
-        viewType: 'board',
-        sortOptions: [],
-        visiblePropertyIds: [],
-        visibleOptionIds: [],
-        hiddenOptionIds: [],
-        collapsedOptionIds: [],
-        filter: {
-          operation: 'and',
-          filters: []
-        },
-        cardOrder: [],
-        columnWidths: {},
-        columnCalculations: {},
-        kanbanCalculations: {},
-        defaultTemplateId: ''
-      },
-      title: 'Board view',
-      createAt: 1655409199321,
-      updateAt: 1655409199321,
-      deleteAt: 0,
-      limited: false
-    }
-  ];
+  let board = new Board(process.env.channelTitle) // board title
 
   const options = {
     method: 'POST',
@@ -135,7 +208,7 @@ function createBoard(workspace) {
       Authorization: 'Bearer ' + process.env.personalAccessToken,
       'X-Requested-With': 'XMLHttpRequest'
     },
-    body: board
+    body: JSON.stringify(board)
   };
 
   return fetch(
@@ -147,19 +220,18 @@ function createBoard(workspace) {
   )
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
+      console.log('return from blocks POST', res);
+
+      return {
+        workspace: workspace,
+        "board": board
+      }
     });
 }
 
-function fetchPlaybookRuns(workspace) {
+function fetchPlaybookRuns(state) {
   let cards = [];
-  //<=7
-  let lessThanWeek = [];
-  //>7 && <=14
-  let greaterThanWeekButLessThanTwoWeeks = [];
-  //>14
-  let greaterThanTwoWeeks = [];
-
+  
   let today = new Date();
 
   const options = {
@@ -180,15 +252,28 @@ function fetchPlaybookRuns(workspace) {
 
     if (res.total_count > 0) {
       res.items.forEach(playbookRun => {
-        if (playbookRun.current_status != "") {
+        if (playbookRun.current_status != "Finished") {
           let durationInDays = eachDayOfInterval({'start': playbookRun.create_at, 'end': today});
+
           console.log('duration in days between create_at & now', durationInDays);
 
-          card = new Card(
-            
+          //TODO : maleable durations
+          //<=7
+          //>7 && <=14
+          //>14
+
+          let viewCardId = "";
+          if (durationInDays <= 7) viewCardId = "";
+          if (durationInDays >= 7 && durationInDays <= 14) viewCardId = "";
+          if (durationInDays > 14) viewCardId = "";
+
+          cards.push(
+            new Card(state.boardId, 
+                     state.viewBlockId, 
+                     viewCardId, 
+                     playbookRun.name)
           )
 
-          if (durationInDays >= 7) 
         }
       })
     }
@@ -200,7 +285,7 @@ function fetchPlaybookRuns(workspace) {
   });
 }
 
-
 function returnNewUUID() {
   return uuidv4().replaceAll('-', '').substring(0, 26);
 }
+
